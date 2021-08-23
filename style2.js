03d2fdcfa4ef7db518a544625565ff5feaffd059
Rivya-Bisht <56182281+Rivya-Bisht@users.noreply.github.com>
2021-08-24 00:20:07 +0530

    Add files via upload

---
+++ style2.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    // slide-up script
+    
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});//working of scroll button
+        $('html').css("scrollBehavior", "auto");
+    });
+
+
+    $('.navbar .menu li a').click(function(){
+       
+        $('html').css("scrollBehavior", "smooth");//buttons smooth transition to tops
+    });
+
+    var a;
+    $('.menu-btn').on('click', function clicked(){
+        if(a===0)
+        {
+            $(".navbar .menu").css({'left':'0%','height':'100%','color':'#331343'});
+                a++
+            }
+        else
+        {
+        $(".navbar .menu").css({'left':'-100%'});a=0;
+        }
+        });
+
+
+});
